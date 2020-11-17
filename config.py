90b2908b3aa0b868877f8086da40dc3e1a2f7186
aliayar <alaettinayar@gmail.com>
2020-11-17 16:45:49 +0100

    database integration

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'noyouwillneverseemysecretsyoubastard'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
