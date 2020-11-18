0f09eeb13c46b30abc7bd125f6bb3933af5e2bf3
aliayar <alaettinayar@gmail.com>
2020-11-18 12:11:57 +0100

    track modification corrected

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'noyouwillneverseemysecretsyoubastard'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
