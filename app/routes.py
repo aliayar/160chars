c2606136a92a539df0cbd3171122c461744404b8
aliayar <alaettinayar@gmail.com>
2020-11-19 16:05:23 +0100

    flask_login implementation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 
 @app.route('/')
@@ -22,6 +24,7 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
