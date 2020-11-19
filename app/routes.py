c8eaa16011ce5f99a2c1000570961915de23d5f9
aliayar <alaettinayar@gmail.com>
2020-11-19 17:01:27 +0100

    couple of missing imports fixed and query_filter fixed with query.filter

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,14 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Ali'}
     posts = [
         {
             'author': {'username': 'Ali'},
@@ -20,7 +19,7 @@ def index():
             'body': 'The Parasite movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -29,7 +28,7 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query_filter_by(username=form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
