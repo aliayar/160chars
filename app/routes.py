f7aaaf17f266f9383838d6db92dd2593e445f8cb
aliayar <alaettinayar@gmail.com>
2020-11-19 16:10:41 +0100

    login page backend implemented

--- app/routes.py
+++ app/routes.py
@@ -24,10 +24,14 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query_filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
