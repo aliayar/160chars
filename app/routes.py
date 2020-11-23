863bee64f7ded6d5af8a1ab66f0d348e2f46aff5
aliayar <alaettinayar@gmail.com>
2020-11-23 14:36:38 +0100

    user last seen and about me models added, user.html updated

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
