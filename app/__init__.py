7d6469a917883ed07e96cab5f56107ce4529a239
aliayar <alaettinayar@gmail.com>
2020-11-24 12:28:30 +0100

    several small bugs across sending error mails implemenmtation fixed

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
+import os
 from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ from app import routes, models, errors
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth=None
-        if app.config['MAIL_USERNAME'] or app_config['MAIL_PASSWORD']:
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
