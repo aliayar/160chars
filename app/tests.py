45cf4c1106af29bbcbfa99bbe62daa6835d6746a
aliayar <alaettinayar@gmail.com>
2020-11-30 16:48:45 +0100

    unit tests setup added

---
+++ app/tests.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+import unittest
+from app import app, db
+from app.models import User, posixpath
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
