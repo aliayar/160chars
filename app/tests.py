c4120edcf7711178a5f0cca976532cfec236a013
aliayar <alaettinayar@gmail.com>
2020-12-01 11:20:21 +0100

    added password hashing and avatar tests

--- app/tests.py
+++ app/tests.py
@@ -11,3 +11,15 @@ class UserModelCase(unittest.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+    def test_password_hashing(self):
+        u = User(username='ali')
+        u.set_password('aliveli')
+        self.assertFalse(u.check_password('veliveli'))
+        self.assertTrue(u.check_password('aliveli'))
+
+    def test_avatar(self):
+        u = User(username='john', email='john@example.com')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
