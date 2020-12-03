19cce95136b3785408f190e398391da6d83f6894
aliayar <alaettinayar@gmail.com>
2020-12-03 16:40:14 +0100

    pagination added to user view method in routes and user view in templates

--- app/routes.py
+++ app/routes.py
@@ -90,12 +90,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Testing #1'},
-        {'author': user, 'body': 'Testing #1'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template ('user.html', user=user, posts=posts, form=form)
+    return render_template ('user.html', user=user, posts=posts.items, form=form,
+                            next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
