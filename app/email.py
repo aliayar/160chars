029840629b71b8a704fbc78aa72e65fef6d6bfc9
aliayar <alaettinayar@gmail.com>
2020-12-07 13:55:48 +0100

    password reset email methods and view templates added

--- app/email.py
+++ app/email.py
@@ -6,3 +6,14 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+
+
+def send_password_reset_email(user):
+    token = user.get.reset.password_token()
+    send_email('[160chars] Reset Your Password',
+                sender=app.config['ADMINS'][0],
+                recipients=[user.email],
+                text_body=render_template('email/reset_password.txt',
+                                        user=user, token=token),
+                html_body=render_template('email/reset_password.html',
+                                        user=user, token=token))
