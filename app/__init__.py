73b34b26b5533c4208052607e19e276316b0552d
aliayar <alaettinayar@gmail.com>
2020-12-08 15:30:36 +0100

    bootstrap-flask added, bootstrap implemented to base.html

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -19,6 +20,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
