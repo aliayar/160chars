79587810b3750ce8e42cc7a1e91405c35f6263aa
aliayar <alaettinayar@gmail.com>
2020-12-15 11:14:50 +0100

    some indentation problem fixed

--- app/search.py
+++ app/search.py
@@ -22,5 +22,5 @@ def query_index(index, query, page, per_page):
         index=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
                 'from': (page - 1) * per_page, 'size': per_page})
-        ids = [int(hit['_id']) for hit in search['hits']['hits']]
-        return ids, search['hits']['total']['value']
+    ids = [int(hit['_id']) for hit in search['hits']['hits']]
+    return ids, search['hits']['total']['value']
