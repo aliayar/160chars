4abb7087288606797be826cac0aefef1c0989b6b
aliayar <alaettinayar@gmail.com>
2020-12-15 15:33:47 +0100

    a typo fixed

--- app/models.py
+++ app/models.py
@@ -96,7 +96,7 @@ def load_user(id):
 class SearchableMixin(object):
     @classmethod
     def search(cls, expression, page, per_page):
-        ids, total = query_index(cls.__tablename___, expression, page, per_page)
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
         if total == 0:
             return cls.query.filter_by(id=0), 0
         when = []
