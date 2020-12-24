b33b42c2a1f91464d1f694dadc82cc6e1ac723a9
Alaettin Ayar <alaettinayar@gmail.com>
2020-12-24 23:54:33 +0100

    a notification model added

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask import current_app
 from datetime import datetime
 from time import time
 import jwt
+import json
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -83,6 +84,8 @@ class User(UserMixin, db.Model):
                                             foreign_keys='Message.recipient_id',
                                             backref='recipient', lazy='dynamic') 
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
 
 
     def __repr__(self):
@@ -164,3 +167,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return f'<Message {self.body}'
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload.json))
