e53940d6ef478d45825a31351fe7a7d2619b2fe0
Alaettin Ayar <alaettinayar@gmail.com>
2020-12-25 23:12:37 +0100

    added messages and notifications to flask shell

--- microblog.py
+++ microblog.py
@@ -5,4 +5,5 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post' :Post}
+    return {'db': db, 'User': User, 'Post' : Post, 'Message: Message',
+            'Notification: Notification'}
