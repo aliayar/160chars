329fffb9e266bd8d1b18b1164757a77f83b6fd41
aliayar <alaettinayar@gmail.com>
2021-01-04 11:48:39 +0100

    small typo fix

--- microblog.py
+++ microblog.py
@@ -5,5 +5,5 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post' : Post, 'Message: Message',
+    return {'db': db, 'User': User, 'Post' : Post, 'Message': Message,
             'Notification: Notification'}
