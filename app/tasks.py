5cda12214147540d39d8a53b987b259a041f96d3
aliayar <alaettinayar@gmail.com>
2021-01-11 16:58:41 +0100

    added application context to tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,14 +1,4 @@
-import time
-from rq import get_current_job
+from app import create_app
 
-def example(seconds):
-    job = get_current_job()
-    print('Starting task')
-    for i in range(seconds):
-        job.meta['progress'] = 100.0 * i / seconds
-        job.save_meta()
-        print(i)
-        time.sleep(1)
-    job.meta['progress'] = 100
-    job.save_meta()
-    print('Task completed!')
+app = create_app()
+app.app_context().push()
